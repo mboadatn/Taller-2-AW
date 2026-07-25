ce9984fe147906279a129565a9fa609e20f88c00
mboada <isaac270307@gmail.com>
2026-07-25 16:29:54 -0500

    feacture EJERCICIO 2

---
+++ ejercicios.js
@@ -0,0 +1,42 @@
+
+
+// EJERCICIO 1:
+
+/* function evaluarEstudiante(notas) {
+    let suma = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        suma += notas[i];
+    }
+
+    let promedio = suma / notas.length;
+
+    if (promedio >= 7) {
+        return "Aprobado";
+    } else {
+        return "Reprobado";
+    }
+}
+const notasEstudiante = [5, 4, 8, 10, 3];
+console.log("Resultado del estudiante: " + evaluarEstudiante(notasEstudiante)); */
+
+// Ejercicio 2:
+
+function contarParesImpares(numeros) {
+    let pares = 0;
+    let impares = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+
+    return { pares: pares, impares: impares };
+}
+
+const listaNumeros = [3, 8, 12, 7, 19, 22, 5];
+const resultadoConteo = contarParesImpares(listaNumeros);
+console.log("Pares: " + resultadoConteo.pares + " | Impares: " + resultadoConteo.impares);
